6fac9cf3ee1038ae13b512488779af1d1d228a4d
sdebesai <serenadebesai@gmail.com>
2025-05-20 13:36:33 -0500

    less noise sensitive turning point function, turning point pipeline debugged

--- reaction_nets.py
+++ reaction_nets.py
@@ -3,9 +3,12 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 import matplotlib.pyplot as plt
-from diffrax import diffeqsolve, ODETerm, Tsit5, SaveAt
+from diffrax import diffeqsolve, ODETerm, Tsit5, SaveAt, Kvaerno3
 import optax  
 import networkx as nx
+import pickle as pkl
+from diffrax import diffeqsolve, ODETerm, Tsit5, SaveAt, Kvaerno3, PIDController
+import optax  
 
 #a general reaction net object. 
 #different methods corresponding to different types of reaction nets
@@ -14,7 +17,7 @@ import networkx as nx
 jax.config.update("jax_enable_x64", True)
 class random_rxn_net:
     def __init__(self, n, m, seed, n_second_order, n_inputs, test=False, A=None, second_order_edge_idxs=None, F_a_idxs=None):
-
+        key = jax.random.PRNGKey(seed)
         if test:
             self.A=A #adjacency matrix for triangle topology
             self.second_order_edge_idxs=second_order_edge_idxs#jnp.array([[0, 1]]) #list of second order edges
@@ -36,6 +39,9 @@ class random_rxn_net:
         nonzero_mask = (self.A != 0) & upper_triangle_mask
         self.F_edge_idxs = jnp.array(jnp.nonzero(nonzero_mask)).T  
 
+        nonzero_mask = (self.A != 0) #& upper_triangle_mask
+        self.F_a_edge_idxs = jnp.array(jnp.nonzero(nonzero_mask)).T 
+
         #setting up inputs and second order edges
         if test:
             self.second_order_edges = jnp.array([[0, 1]]) #second order reactions
@@ -43,30 +49,38 @@ class random_rxn_net:
             self.second_order_edge_prods = jnp.array([[1, 2]]) #jnp.array([[1, 1]]) 
         else:
             #edges for inputs
-            idxs = np.random.choice(len(self.F_edge_idxs), size=n_inputs, replace=False)
-            self.F_a_idxs = jnp.array(self.F_edge_idxs[idxs])
+            key, subkey = jax.random.split(key)
+            idxs = jax.random.choice(subkey, jnp.arange(len(self.F_a_edge_idxs)), shape=(n_inputs, ), replace=False)
+            self.F_a_idxs = jnp.array(self.F_a_edge_idxs[idxs])
             if self.n_second_order>0:
-                idxs = np.random.choice(len(self.edge_idxs), size=n_second_order, replace=False)
-                self.second_order_edges = jnp.array(self.edge_idxs[idxs])
+                if second_order_edge_idxs == None:
+                    key, subkey = jax.random.split(key)
+                    idxs = jax.random.choice(subkey, jnp.arange(len(self.edge_idxs)), shape=(n_second_order, ), replace=False)
+                    self.second_order_edges = jnp.array(self.edge_idxs[idxs])
+                else:
+                    self.second_order_edges=second_order_edge_idxs
 
                 self.second_order_edge_prods = jnp.zeros((self.n_second_order, 2), dtype=int)
                 self.second_order_edge_reactants = jnp.zeros((self.n_second_order, 2), dtype=int)
                 
                 for i, edge in enumerate(self.second_order_edges):
                     #map to some indices
-                    key = jax.random.PRNGKey(0)
+                    #will always generate the same second order edges right now
+                    
                     key, subkey1, subkey2 = jax.random.split(key, 3)
                     reactant2=jax.random.randint(key=subkey1, shape=(1,), minval=0, maxval=self.n-1, dtype=int)[0]
-                    key, subkey=jax.random.split(key)
                     prod2=jax.random.randint(key=subkey2, shape=(1,), minval=0, maxval=self.n-1, dtype=int)[0]
 
                     #randomly select the second species in the second order reaction 
                     reactants=jnp.array([edge[0], reactant2])
                     prods=jnp.array([edge[1], prod2])
-                   
+
                     self.second_order_edge_reactants=self.second_order_edge_reactants.at[i].set(reactants.copy())
                     self.second_order_edge_prods=self.second_order_edge_prods.at[i].set(prods.copy())
-    
+            else:
+                self.second_order_edges=jnp.array([[0, 0]])
+                self.second_order_edge_reactants=jnp.array([[0, 0]])
+                self.second_order_edge_prods=jnp.array([[0, 0]])
     #right now this does NOT assume that the second order edges are bidirectional
     def rxn_net_dynamics(self, t, y, params):
         #jax.debug.print('log concentrations: {x}', x=y)
@@ -102,104 +116,133 @@ class random_rxn_net:
         #reshape arrays accordingly to create rates matrix 
         W=jnp.exp(E - B_params + 0.5*F_params+0.5*F_a_params)
         rows, cols=self.edge_idxs.T
-
         mask = jnp.zeros((self.n, self.n), dtype=bool)
-
         mask = mask.at[rows, cols].set(True)
-
         W=W * mask
-
-        #set second order edges to 0
-        W_first_order=W.copy()
         
-        if self.n_second_order > 0:
-            cols, rows=self.second_order_edges.T
+        #compute W first order 
+        def compute_W_first_order(inputs):
+            second_order_edges, W_first_order=inputs
+            #jax.debug.print('computing W first order')
+            #jax.debug.print('edges: {x}', x=second_order_edges)
+            cols, rows=second_order_edges.T
             #jax.debug.print('rows: {x}', x=rows)
             #jax.debug.print('cols: {x}', x=cols)
             W_first_order=W_first_order.at[rows, cols].set(0)
 
-        #jax.debug.print('W_first_order: {x}', x=W_first_order)
+            return W_first_order
+        
+        def skip_W_first_order(inputs):
+            second_order_edges, W_first_order=inputs
+            #jax.debug.print('skipping W first order')
+            #jax.debug.print('edges: {x}', x=second_order_edges)
+            return W_first_order
+        
+        #set second order edges to 0
+        W_first_order=W.copy()
+        comp_W_cond=self.n_second_order > 0 
+        W_first_order=jax.lax.cond(comp_W_cond, compute_W_first_order, skip_W_first_order, (self.second_order_edges, W_first_order))
 
+        #jax.debug.print('W_first_order: {x}', x=W_first_order)
+        #jax.debug.print('W: {x}', x=W_first_order)
+        
+        #functions to process second order reactions
+        def process_reactant(inputs):
+            i, j, reactants, idx_i, idx_f, dydt = inputs
+            term = 1
+                        
+            def i_eq_j_branch():
+                return -1
+            def i_not_eq_j_branch():
+                return 0 
+                        
+            c=jax.lax.cond(i==j, i_eq_j_branch, i_not_eq_j_branch)
+                            
+            for k in reactants:
+                term=term*y[k] #multiply by the concentration of the other species in the reaction 
+            dydt=dydt.at[i].set(dydt[i] + c*term*W[idx_f, idx_i])
+            #jax.debug.print('second order prod contribution: {x}*{y}*{z}={u}', x=c, y=term, z=W[idx_f, idx_i], u=c*term*W[idx_f, idx_i])
+            return dydt
+            
+        #skip over chaperones
+        def skip_reactant(inputs):
+            i, j, reactants, idx_i, idx_f, dydt=inputs
+            return dydt
+            
+        def process_product(inputs):
+            i, j, reactants, idx_i, idx_f, dydt = inputs
+            term = 1
+                            
+            def i_eq_j_branch():
+                return 1
+            def i_not_eq_j_branch():
+                return 0 
+                        
+            c=jax.lax.cond(i==j, i_eq_j_branch, i_not_eq_j_branch)
+
+            for k in reactants:
+                term=term*y[k] #multiply by the concentration of the other species in the reaction
+                            
+            dydt=dydt.at[i].set(dydt[i] + c*term*W[idx_f, idx_i])
+
+            #jax.debug.print('second order reactant contribution: {x}*{y}*{z}={u}', x=c, y=term, z=W[idx_f, idx_i], u=c*term*W[idx_f, idx_i])
+
+            return dydt
+            
+        def skip_product(inputs):
+            i, j, reactants, idx_i, idx_f, dydt=inputs
+            return dydt
+            
+        def second_order_rxns(inputs):
+            i, second_order_edges, second_order_edge_reactants, second_order_edge_prods, dydt=inputs
+            #second order reactions
+            #jax.debug.print('before second order: {x}', x=dydt)
+            #jax.debug.print('computing second order contributions')
+            for e, edge in enumerate(second_order_edges):
+                idx_i=edge[0]
+                idx_f=edge[1]
+              
+                #there is a term for each reactant in the reaction if the reactant is the same as the species being considered 
+
+                reactants=second_order_edge_reactants[e]
+                products=second_order_edge_prods[e]
+
+                #jax.debug.print('second order reactants {x}:', x=reactants)
+                #jax.debug.print('second order prods {x}:', x=products)
+                
+                #iterate through reactants 
+                for j in reactants:
+                    skip=jnp.where(products == j, 1, 0).sum() > 0
+                    #if the reactant is also a product then it's a chaperone, in which case we skip it
+                    #otherwise, we compute a term 
+                    dydt=jax.lax.cond(skip, skip_reactant, process_reactant, (i, j, reactants, idx_i, idx_f, dydt))
+                    
+                #there is term for each product in the reaction if the reactant is the same as the species being considered
+                for j in products:   
+                    skip=jnp.where(reactants == j, 1, 0).sum() > 0
+                    dydt=jax.lax.cond(skip, skip_product, process_product, (i, j, reactants, idx_i, idx_f, dydt))
+                #jax.debug.print('updated dydt: {x}', x=dydt)
+                #jax.debug.print('dydt:{x}',x=dydt)
+            return dydt
+            
+        def skip_second_order(inputs):
+            i, second_order_edges, second_order_edge_reactants, second_order_edge_prods, dydt=inputs
+            return dydt
+        
+        #iterate through each species and compute dydt
         dydt = jnp.zeros(self.n)
         for i in range(self.n):  
             #jax.debug.print('i: {x}', x=i)
+            #jax.debug.print('positive first order: {x}', x=W_first_order[i] @ y)
+            #jax.debug.print('negative first order: {x}', x=jnp.sum(W_first_order[:, i] * y[i]))
             #jax.debug.print('first order contribution: {x}', x=dydt[i] + W_first_order[i] @ y - jnp.sum(W_first_order[:, i] * y[i]))
+
             #contributions from first order 
-            dydt=dydt.at[i].set(dydt[i] + W_first_order[i] @ y - jnp.sum(W_first_order[:, i] * y[i]))
-            if self.n_second_order > 0:
-                #second order reactions
-                #jax.debug.print('before second order: {x}', x=dydt)
-                #jax.debug.print('computing second order contributions')
-                for e, edge in enumerate(self.second_order_edges):
-                    idx_i=edge[0]
-                    idx_f=edge[1]
-                
-                    c=0
-                
-                    #there is a term for each reactant in the reaction if the reactant is the same as the species being considered 
+            dydt=dydt.at[i].set(dydt[i] + W_first_order[i] @ y - jnp.sum(W_first_order[:, i] * y[i])) #first order reactions
+            process_second_order=self.n_second_order > 0
+            dydt=jax.lax.cond(process_second_order, second_order_rxns, skip_second_order, (i, self.second_order_edges, self.second_order_edge_reactants, self.second_order_edge_prods, dydt)) #second order reactions
 
-                    reactants=self.second_order_edge_reactants[e]
-                    products=self.second_order_edge_prods[e]
-  
-                    def process_reactant(inputs):
-                            j, dydt = inputs
-                            term = 1
-                        
-                            def i_eq_j_branch():
-                                return -1
-                            def i_not_eq_j_branch():
-                                return 0 
-                        
-                            c=jax.lax.cond(i==j, i_eq_j_branch, i_not_eq_j_branch)
-                            
-                            for k in reactants:
-                                term=term*y[k] #multiply by the concentration of the other species in the reaction 
-                            dydt=dydt.at[i].set(dydt[i] + c*term*W[idx_f, idx_i])
-
-                            return dydt
-                    #skip over chaperones
-                    def skip_reactant(inputs):
-                        j, dydt=inputs
-                        return dydt
-
-                    def process_product(inputs):
-                            j, dydt = inputs
-                            term = 1
-                            #c = 1 if i==j else 0
-                            def i_eq_j_branch():
-                                return 1
-                            def i_not_eq_j_branch():
-                                return 0 
-                        
-                            c=jax.lax.cond(i==j, i_eq_j_branch, i_not_eq_j_branch)
-
-                            for k in reactants:
-                                term=term*y[k] #multiply by the concentration of the other species in the reaction
-                            
-                            dydt=dydt.at[i].set(dydt[i] + c*term*W[idx_f, idx_i])
-
-                            return dydt
-
-                    def skip_product(inputs):
-                        j, dydt=inputs
-                        return dydt
-             
-                    for j in reactants:
-                        skip=jnp.where(products == j, 1, 0).sum() > 0
-                        dydt=jax.lax.cond(skip, skip_reactant, process_reactant, (j, dydt))
-                    
-                    #there is term for each product in the reaction if the reactant is the same as the species being considered
-                    for j in products:   
-               
-                        #jax.debug.print('j: {x}', x=j)    
-                        #if jnp.sum((jnp.isin(j,reactants)).astype(jnp.int32)) > 0:
-                        skip=jnp.where(reactants == j, 1, 0).sum() > 0
-                        dydt=jax.lax.cond(skip, skip_product, process_product, (j, dydt))
-
-                    #jax.debug.print('updated dydt: {x}', x=dydt)
-            #jax.debug.print('dydt:{x}',x=dydt)
-        #jax.debug.print('dlog(y)dt:{x}\n',x=dydt/y)
-                
+            #jax.debug.print('dy[i]dt:{x}',x=dydt)      
         return dydt / y
     
     def integrate(self, solver, stepsize_controller, t_points, dt0, initial_conditions, args, max_steps):
@@ -210,5 +253,4 @@ class random_rxn_net:
         solution = diffeqsolve(term, solver=solver, stepsize_controller=stepsize_controller, t0=t_points[0], t1=t_points[-1], dt0=dt0, y0=initial_conditions, args=args, saveat=SaveAt(ts=t_points), max_steps=max_steps)
         #solution = diffeqsolve(term, solver=solver, t0=t_points[0], t1=t_points[-1], dt0=dt0, y0=initial_conditions, args=args, saveat=SaveAt(ts=t_points), max_steps=max_steps)
         return solution
-
 
--- turning_points.py
+++ turning_points.py
@@ -13,49 +13,91 @@ import random
 from reaction_nets import random_rxn_net
 from rxn_nets_old import rxn_net
 from flax import serialization
+import whittaker_smooth
+import scipy
 
 def profile(rxn, params, initial_conditions, all_features, solver, stepsize_controller, t_points, dt, max_steps):
     E, B, F=params
     solns=[]
     for F_a in all_features:
         sol_F_a=rxn.integrate(solver=solver, stepsize_controller=stepsize_controller, t_points=t_points, dt0=dt, initial_conditions=initial_conditions, args=(E, B, F, F_a,), max_steps=max_steps) 
-        #print(jnp.sum(jnp.exp(sol_F_a.ys), axis=1))
         solns.append(sol_F_a.ys[-1].copy())
     return jnp.exp(jnp.array(solns))
 
-def count_turning_points(arr: np.ndarray):
-    # Assumes arr is a 1D NumPy array
-    # Remove consecutive duplicates (flattened plateaus)
-    diff_indices = np.where(np.diff(arr) != 0)[0] + 1
-    unique_locs = np.concatenate(([0], diff_indices))
+def count_turning_points(data):
+    peaks, props = scipy.signal.find_peaks(data, plateau_size=1, prominence=0.1)
+    troughs,props = scipy.signal.find_peaks(-data, plateau_size=1, prominence=0.1)
 
-    arr_simple = arr[unique_locs]
-    dx = np.diff(arr_simple)
-    
-    # Identify where the sign of the slope changes (i.e., turning points)
-    turning_mask = dx[1:] * dx[:-1] < 0
+    return peaks.shape[0] + troughs.shape[0]
 
-    # Map back to original indices — use the first index of each plateau
-    return np.count_nonzero(turning_mask)
+def gen_profiles(fname, n, m, seeds, n_second_order, n_inputs, second_order_edge_idxs, initial_conditions, all_features, solver, stepsize_controller, t_points, dt,max_steps):
+    with open(fname, "rb") as f:
+        params_rand = pkl.load(f)
+    f.close()
 
-def gen_rand_rxn_nets(seed, n_nets, n, m, n_second_order, n_inputs, test, A, second_order_edge_idxs, F_a_idxs):
-    rxn_nets=[]
-    for i in range(n_nets):
-        rxn=random_rxn_net(n, m,seed,n_second_order, n_inputs, test, A, second_order_edge_idxs, F_a_idxs)
-        rxn_nets.append(rxn)
-   
-    with open("rxn_nets.msgpack", "wb") as f:
-        f.write(serialization.to_bytes(rxn_nets))
-
-def gen_profiles(fname, params, all_features, solver, stepsize_controller, t_points, dt,max_steps):
-    with open("rxn_nets.msgpack", "rb") as f:
-        obj = serialization.from_bytes(rxn_nets, f.read())
-
-    n_profiles=len(rxn_nets)
-    dist_tps=np.zeros(n_profiles)
-    for i, rxn in enumerate(rxn_nets):
+    n_profiles=2#len(params_rand)
+    dist_tps=jnp.zeros(n_profiles * n)
+    solns_all=jnp.zeros((n_profiles, all_features.shape[0], n))
+    counter=0
+    for i, params in enumerate(params_rand[0:n_profiles]):
+        jax.debug.print('i: {x}', x=i)
+        jax.debug.print('params: {x}', x=params)
         #gen reaction net
+        seed=int(seeds[i])
         
+        rxn=random_rxn_net(n, m, seed, n_second_order, n_inputs, test=False, A=None, second_order_edge_idxs=second_order_edge_idxs, F_a_idxs=None)
         solns=profile(rxn, params, initial_conditions, all_features, solver, stepsize_controller, t_points, dt, max_steps)
-        n_tps=count_turning_points(solns.ys)
-        dist_tps[i]=n_tps
+        solns_all = solns_all.at[i].set(solns.copy())
+
+        for j, species_prof in enumerate(solns.T):
+            n_tps=count_turning_points(species_prof)
+            dist_tps.at[counter].set(n_tps)
+            counter+=1
+
+    return dist_tps, solns_all
+
+def main():
+    n=6
+    m=n*(n-1)/2
+    seeds=np.arange(1, 4001)
+    seeds = seeds.reshape(4, 1000)
+    initial_conditions=jnp.log(jnp.array([1/6, 1/6, 1/6, 1/6, 1/6, 1/6]))
+    all_features=jnp.linspace(-20, 20, 100)
+    t_points=jnp.linspace(0, 20, 200)
+    solver= Kvaerno3()
+    stepsize_controller=PIDController(
+    rtol=1e-6,     
+    atol=1e-9,   
+    dtmin=1e-11,   
+    dtmax=1e-1     
+    )
+    dt=0.1
+    
+    max_steps=10000000
+    second_order_edge_idxs=[jnp.array([0, 1]), jnp.array([[0, 1], [4, 1]]), jnp.array([[0, 1], [4, 1], [2, 3]]), jnp.array([[0, 1], [4, 1], [2, 3], [4, 5]])]
+    n_second_order_all=jnp.array([0,1,2,3])
+
+    n_inputs=1
+
+    params_file=f'data/turning_points/params_all_N0'
+
+    rel=0
+    for i, (n_second_order, second_order_edge) in enumerate(zip(n_second_order_all, second_order_edge_idxs)):
+        dist, solns_all=gen_profiles(params_file, n, m, seeds[i], n_second_order, n_inputs,second_order_edge, initial_conditions, all_features, solver, stepsize_controller, t_points, dt,max_steps)
+
+        #save result
+        f=open(f'data/turning_points/dist_N{n}_M{m}_S{n_second_order}_distributions', 'wb')
+        pkl.dump(dist, f)
+        f.close()
+
+        #save result
+        f=open(f'data/turning_points/dist_N{n}_M{m}_S{n_second_order}_profiles', 'wb')
+        pkl.dump(solns_all, f)
+        f.close()
+        rel+=1
+
+if __name__ == "__main__":
+    main()
+
+
+
